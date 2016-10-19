c7da0f4be2af77ba9baad2711b0f148eb644dc41
YAP! <GMADDA200@caledonian.ac.uk>
2016-10-19 11:45:56 +0100

    Implemented Perlin noise generator

--- Terra Nova/Common.h
+++ Terra Nova/Common.h
@@ -1,6 +1,16 @@
 #pragma once
 
+//utility
 #include "stdafx.h"
 #include <iostream>
 #include <vector>
+
+//sfml
 #include <SFML\Graphics.hpp>
+
+
+//perlin noise
+#include <random>
+#include <array>
+#include <algorithm>
+#include <numeric> 
---
+++ Terra Nova/Perlin.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "Common.h"
+
+//from: http://blog.kazade.co.uk/2014/05/a-public-domain-c11-1d2d3d-perlin-noise.html
+
+class Perlin {
+public:
+	Perlin(uint32_t seed = 0);
+
+	double noise(double x) const { return noise(x, 0, 0); }
+	double noise(double x, double y) const { return noise(x, y, 0); }
+	double noise(double x, double y, double z) const;
+
+private:
+	std::array<int, 512> p;
+};
+
+class PerlinOctave {
+public:
+	PerlinOctave(int octaves, uint32_t seed = 0);
+
+	double noise(double x) const { return noise(x, 0, 0); }
+	double noise(double x, double y) const { return noise(x, y, 0); }
+	double noise(double x, double y, double z) const;
+
+private:
+	Perlin perlin_;
+	int octaves_;
+};
